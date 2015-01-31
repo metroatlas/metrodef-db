bcbb3b6a109d745f1018875b14cd9d4b691027c3
jgbryan <jordangbryman@gmail.com>
2015-01-30 17:53:02 -0800

    adding the scripts used to manipulate the data, generate preliminary visualizations, and construct adjacency matrix

---
+++ data_cleaning.R
@@ -0,0 +1,105 @@
+library(igraph)
+
+setwd("~/Desktop/BillLane_Research")
+dat = read.csv("clean.csv", fileEncoding="latin1")
+county.pop = read.csv("county_pop.csv", fileEncoding="latin1")
+
+county.pop$County = as.character(county.pop$County)
+county.pop$State = as.character(county.pop$State)
+# Remove leading space in state names
+county.pop$State = substr(county.pop$State, 2, nchar(county.pop$State))
+
+PadZeros = function(col, size) {
+  # Convert column to string
+  newCol = as.character(col)
+  
+  # Pad zeros
+  for(i in 1:size-1){
+    newCol[nchar(newCol)==i] = paste0(paste0(rep("0",size-i),collapse=""), newCol[nchar(newCol)==i])
+  }
+  
+  return(newCol)
+}
+
+dat[,1] = PadZeros(dat[,1], 2)
+dat[,2] = PadZeros(dat[,2], 3)
+dat[,3] = PadZeros(dat[,3], 3)
+dat[,4] = PadZeros(dat[,4], 3)
+
+# Remove rows with Puerto Rico residence
+dat_nopr = dat[dat$RES_State != "Puerto Rico",]
+
+# Remove rows with people working overseas (e.g. military)
+dat_nomil = dat_nopr[as.numeric(dat_nopr$WRK_State_FIPS_Code) <= 56,]
+
+# Finalize data and store in properly named variable
+census.data = dat_nomil
+for(i in 7:ncol(census.data)){
+  census.data[,i] = as.character(census.data[,i])
+}
+dat_nomil = NULL
+
+# Collect state names
+state.names = unique(census.data$RES_State)
+
+# Add percentage column to census.data
+num.pct = rep(0, nrow(census.data))
+moe.pct = rep(0,nrow(census.data))
+for(i in 1:nrow(county.pop)){
+  print(paste0("Processing Population of County ", i, " of 3143"))
+  indices = intersect(which(census.data$RES_County == county.pop$County[i]), 
+            which(census.data$RES_State == county.pop$State[i]))
+  
+  pop = county.pop$April.1.2010.Census[i]
+  
+  num.pct[indices] = (census.data$Number[indices] / pop)
+  
+  moe.pct[indices] = (census.data$MOE[indices] / pop)
+}
+
+census.data$Number_PCT = num.pct
+census.data$MOE_PCT = moe.pct
+
+# Create function that makes commuting adjacency matrix for a given state
+CMatrix = function(state.name, census.data, threshold=0.15, weights=FALSE){
+  
+  # Gather commuting data only for particular state
+  state.data = census.data[census.data$RES_State == state.name,]
+  
+  # Collect county names
+  res.county.names = unique(state.data$RES_County)
+  wrk.county.names = unique(state.data$WRK_County)
+  
+  # Merge work and county names
+  county.names = sort(union(res.county.names, wrk.county.names))
+  
+  # Create adjacency matrix
+  adj.mat = matrix( , nrow = length(county.names), ncol = length(county.names))
+  adj.mat[,] = 0
+  
+  # Fill in adjacency matrix
+  for(i in 1:nrow(state.data)){
+    comm.pct = state.data$Number_PCT[i] - state.data$MOE_PCT[i]
+    res.indx = which(county.names==state.data$RES_County[i])
+    wrk.indx= which(county.names==state.data$WRK_County[i])
+    if(comm.pct >= threshold && weights == FALSE) {
+      adj.mat[res.indx, wrk.indx] = 1
+    } else if(comm.pct >= threshold && weights == TRUE){
+      adj.mat[res.indx, wrk.indx] = comm.pct
+    }
+  }
+  
+  # Strip adjacency matrix of columns/rows that are entirely zero
+  connected.rows = apply(adj.mat, 2, function(x){any(x>0)})
+  connected.cols = apply(adj.mat, 1, function(x){any(x>0)})
+  connected = connected.rows | connected.cols
+  
+  # Collect subset of county.names
+  labs = county.names[connected]
+  
+  # Plot the adjacency matrix
+  plot(graph.adjacency(adj.mat[connected,connected], mode="directed", weighted=TRUE), vertex.size=10, vertex.label=labs,
+       vertex.label.cex=0.6, edge.arrow.size=0.15)
+  
+  return(adj.mat)
+}
---
+++ hcluster.R
@@ -0,0 +1,147 @@
+# Create function that makes commuting adjacency matrix for a given state, hierarchically clusters
+# the counties based on commuting pct, and then displays a simple node-edge visualization of the 
+# result for a desired number of clusters
+Cluster = function(state.name, census.data, type="single", num.clusters=10){
+  
+  # Gather commuting data only for particular state
+  state.data = census.data[census.data$RES_State == state.name,]
+  
+  # Collect county names
+  res.county.names = unique(state.data$RES_County)
+  wrk.county.names = unique(state.data$WRK_County)
+  
+  # Merge work and county names
+  county.names = sort(union(res.county.names, wrk.county.names))
+  
+  # Create adjacency matrix
+  adj.mat = matrix( , nrow = length(county.names), ncol = length(county.names))
+  adj.mat[,] = 0
+  
+  # Fill in adjacency matrix
+  for(i in 1:nrow(state.data)){
+    comm.pct = state.data$Number_PCT[i] - state.data$MOE_PCT[i]
+    res.indx = which(county.names==state.data$RES_County[i])
+    wrk.indx= which(county.names==state.data$WRK_County[i])
+    adj.mat[res.indx, wrk.indx] = comm.pct
+  }
+  
+  # Strip adjacency matrix of columns/rows that are entirely zero (or close enough to zero)
+  connected.rows = apply(adj.mat, 2, function(x){any(x>0.0001)})
+  connected.cols = apply(adj.mat, 1, function(x){any(x>0.0001)})
+  connected = connected.rows | connected.cols
+  
+  # Collect subset of county.names
+  labs = county.names[connected]
+  indices = 1:length(county.names)
+  indices = indices[connected]
+  adj.mat = adj.mat[connected,connected]
+  colnames(adj.mat) = labs
+  rownames(adj.mat) = labs
+  
+  # Hierarchically cluster according to distances
+  links = HCluster(adj.mat, type, num.clusters,labs)
+  
+  # Produce plot of the clusters
+  #ClusterViz(cluster.list, labs)
+  
+  return(links)
+}
+
+HCluster = function(adj.mat, type="single", num.clusters=10,labs) {
+  # This function takes in an adjacency matrix between counties, a list of the
+  # county names, and a 'type' argument which specifies how the clustering is to be performed.
+  # Distances need not be recalculated in this function, only retreived from the adjacency matrix.
+  
+  # Initialize clusters to be all counties
+  cluster.list = list()
+  for(i in 1:nrow(adj.mat)){
+    cluster.list[[i]] = i
+  }
+  
+  while(length(cluster.list)>num.clusters){
+    print(paste0("Number of clusters: ", length(cluster.list)))
+    dist.mat = matrix(,nrow=length(cluster.list), ncol=length(cluster.list))
+    dist.mat[,] = 0
+    # Calculate distance between clusters
+    for(i in 2:length(cluster.list)){
+      for(j in 1:(i-1)){
+        # Retrieve candidate clusters
+        cluster1 = cluster.list[[i]]
+        cluster2 = cluster.list[[j]]
+        # Find distance between all of the members of the clusters
+        cluster.dist = adj.mat[unlist(cluster1),unlist(cluster2)]
+        if(type == "single"){
+          # In this case, our "distance" actually increases with similarity.
+          # Thus, single linkage should consider the cluster-distance to be the maximum.
+          dist = max(cluster.dist)
+        } else if(type == "complete"){
+          dist = min(cluster.dist)
+        } else if(type == "average"){
+          dist = mean(cluster.dist)
+        }
+        dist.mat[i,j] = dist
+      }
+    }
+    
+    # Find index of maximum element of the distance matrix
+    max.dist = max(dist.mat)
+    if(max.dist==0){return(cluster.list)}
+    max.ind = which(dist.mat == max.dist, arr.ind=TRUE)
+    
+    # Join "nearest" clusters
+    for(m in 1:nrow(max.ind)){
+      cluster.list[[max.ind[m,1]]] = list(cluster.list[[max.ind[m,1]]], cluster.list[[max.ind[m,2]]])
+      cluster.list[[max.ind[m,2]]] = NULL
+      max.ind[max.ind >= max.ind[m,2]] = max.ind[max.ind >= max.ind[m,2]] - 1
+    }
+  }
+  # Make links list
+  links = ListLinks(cluster.list, labs, adj.mat, max.dist)
+  return(links)
+}
+
+ClusterViz = function(cluster.list, labs){
+  adj.mat = matrix(,nrow=length(labs), ncol=length(labs))
+  adj.mat[,] = 0
+  for(i in 1:length(cluster.list)){
+    curr.clust = unlist(cluster.list[[i]])
+    for(j in 1:length(curr.clust)){
+      for(k in 1:length(curr.clust))
+        if(curr.clust[j] != curr.clust[k]){
+          adj.mat[curr.clust[j],curr.clust[k]] = 1
+        }
+    }
+  }
+  plot(graph.adjacency(adj.mat, mode="undirected"), vertex.size=10, vertex.label=labs,
+  vertex.label.cex=0.6, edge.arrow.size=0.15)
+}
+
+ListLinks = function(cluster.list, labs, adj.mat, dist.threshold){
+  links = list()
+  Source = c()
+  Target = c()
+  Cluster.id = c()
+  for(i in 1:length(cluster.list)){
+    curr.clust = unlist(cluster.list[[i]])
+    for(j in 1:length(curr.clust)){
+      for(k in 1:length(curr.clust))
+        if(curr.clust[j] != curr.clust[k]){
+          if(adj.mat[curr.clust[j],curr.clust[k]] >= dist.threshold){
+            Source = c(Source, labs[curr.clust[j]])
+            Target = c(Target, labs[curr.clust[k]])
+            Cluster.id = c(Cluster.id, i)
+          } else if(adj.mat[curr.clust[k],curr.clust[j]] >= dist.threshold){
+            Source = c(Source, labs[curr.clust[k]])
+            Target = c(Target, labs[curr.clust[j]])
+            Cluster.id = c(Cluster.id, i)
+          }
+        }
+    }
+  }
+  
+  links$"source" = Source
+  links$"target" = Target
+  links$"cluster" = Cluster.id
+  
+  return(links)
+}
