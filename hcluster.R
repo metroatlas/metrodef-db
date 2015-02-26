67cbc01e28531ddeb51257efe5bcd388ed7d6d36
jgbryan <jordangbryman@gmail.com>
2015-02-26 13:13:56 -0800

    uploading correct clustering algorithm as well as update to clustering function that outputs a cluster assignment table

--- hcluster.R
+++ hcluster.R
@@ -1,7 +1,7 @@
 # Create function that makes commuting adjacency matrix for a given state, hierarchically clusters
 # the counties based on commuting pct, and then displays a simple node-edge visualization of the 
 # result for a desired number of clusters
-Cluster = function(state.name, census.data, type="single", num.clusters=10, county.pop){
+Cluster = function(state.name, census.data, type="single", num.clusters=10, county.pop, output="links"){
   
   # Gather commuting data only for particular state
   state.data = census.data[census.data$RES_State == state.name,]
@@ -23,7 +23,10 @@ Cluster = function(state.name, census.data, type="single", num.clusters=10, coun
     wrk.indx= which(county.names==state.data$WRK_County[i])
     adj.mat[res.indx, wrk.indx] = comm.pct
   }
-
+  
+  # Make any negative entries zero by default
+  adj.mat[adj.mat < 0] = 0
+  
   # Strip adjacency matrix of columns/rows that are entirely zero (or close enough to zero)
   connected.rows = apply(adj.mat, 2, function(x){any(x>0.0001)})
   connected.cols = apply(adj.mat, 1, function(x){any(x>0.0001)})
@@ -33,11 +36,11 @@ Cluster = function(state.name, census.data, type="single", num.clusters=10, coun
   labs = county.names[connected]
   indices = 1:length(county.names)
   indices = indices[connected]
-  adj.mat = adj.mat[connected,connected]
+  adj.mat = adj.mat[indices,indices]
   colnames(adj.mat) = labs
   rownames(adj.mat) = labs
   # Hierarchically cluster according to distances
-  links = HCluster(adj.mat, type, num.clusters,labs, county.pop)
+  links = HCluster(adj.mat, type, num.clusters,labs, county.pop, output)
  
   # Produce plot of the clusters
   # ClusterViz(cluster.list, labs)
@@ -45,7 +48,7 @@ Cluster = function(state.name, census.data, type="single", num.clusters=10, coun
   return(links)
 }
 
-HCluster = function(adj.mat, type="single", num.clusters=10, labs, county.pop) {
+HCluster = function(adj.mat, type="single", num.clusters=10, labs, county.pop, output) {
   # This function takes in an adjacency matrix between counties, a list of the
   # county names, and a 'type' argument which specifies how the clustering is to be performed.
   # Distances need not be recalculated in this function, only retreived from the adjacency matrix.
@@ -55,6 +58,10 @@ HCluster = function(adj.mat, type="single", num.clusters=10, labs, county.pop) {
   for(i in 1:nrow(adj.mat)){
     cluster.list[[i]] = i
   }
+  
+  cluster.id.table = matrix(,nrow=length(labs),ncol=(length(labs)-num.clusters+1))
+  cluster.id.table[,] = 0
+  cluster.id.table[,1] = 1:length(labs)
   
   while(length(cluster.list)>num.clusters){
     print(paste0("Number of clusters: ", length(cluster.list)))
@@ -69,18 +76,28 @@ HCluster = function(adj.mat, type="single", num.clusters=10, labs, county.pop) {
         # Find distance between all of the members of the clusters
         cluster.dist = matrix(,nrow=length(cluster1), ncol=length(cluster2))
         cluster.dist[,] = 0
-        for(k in length(cluster1)){
-          for(t in length(cluster2)){
-            cluster.dist[k,t] = adj.mat[cluster1[k],cluster2[t]]
-          }
-        }
         if(type == "single"){
           # In this case, our "distance" actually increases with similarity.
           # Thus, single linkage should consider the cluster-distance to be the maximum.
+          for(k in 1:length(cluster1)){
+            for(t in 1:length(cluster2)){
+              cluster.dist[k,t] = max(adj.mat[cluster1[[k]],cluster2[[t]]],adj.mat[cluster2[[t]],cluster1[[k]]])
+            }
+          }
           dist = max(cluster.dist)
         } else if(type == "complete"){
+          for(k in 1:length(cluster1)){
+            for(t in 1:length(cluster2)){
+              cluster.dist[k,t] = min(adj.mat[cluster1[k],cluster2[t]],adj.mat[cluster2[t],cluster1[k]])
+            }
+          }
           dist = min(cluster.dist)
         } else if(type == "average"){
+          for(k in 1:length(cluster1)){
+            for(t in 1:length(cluster2)){
+              cluster.dist[k,t] = (adj.mat[cluster1[[k]],cluster2[[t]]]+adj.mat[cluster2[[t]],cluster1[[k]]])/2
+            }
+          }
           dist = mean(cluster.dist)
         }
         dist.mat[i,j] = dist
@@ -90,25 +107,56 @@ HCluster = function(adj.mat, type="single", num.clusters=10, labs, county.pop) {
     # Find index of maximum element of the distance matrix
     max.dist = max(dist.mat)
     print(max.dist)
-    print(length(cluster.list))
     if(max.dist==0){
       links = ListLinks(cluster.list, labs, adj.mat, max.dist, county.pop)
       return(links)
     }
     max.ind = which(dist.mat == max.dist, arr.ind=TRUE)
-    print(max.ind)
-    print(max.dist)
     # Join "nearest" clusters
     for(m in 1:nrow(max.ind)){
       cluster.list[[max.ind[m,1]]] = list(cluster.list[[max.ind[m,1]]], cluster.list[[max.ind[m,2]]])
       cluster.list[[max.ind[m,2]]] = NULL
       max.ind[max.ind >= max.ind[m,2]] = max.ind[max.ind >= max.ind[m,2]] - 1
     }
+    
+    # Reflect joining in the cluster table
+    for(i in 1:length(cluster.list)){
+      curr.clust = unlist(cluster.list[[i]])
+      for(j in 1:length(curr.clust)){
+        cluster.id.table[curr.clust[j],(length(labs)-length(cluster.list)+1)] = i
+      }
+    }
   }
   
   # Make links list
-  links = ListLinks(cluster.list, labs, adj.mat, max.dist, county.pop)
-  return(links)
+  if(output == "links"){
+    links = ListLinks(cluster.list, labs, adj.mat, max.dist, county.pop)
+    return(links)
+  }
+
+  # Make cluster column
+  if(output == "cluster.col"){
+    cluster.id.table = as.data.frame(cluster.id.table)
+    fips.codes = rep(0,length(labs))
+    for(k in 1:length(labs)){
+      fips.codes[k] = paste0(census.data$RES_State_FIPS_Code[census.data$RES_County==labs[k]][1],
+                            census.data$RES_County_FIPS_Code[census.data$RES_County==labs[k]][1])
+    }
+    View(fips.codes)
+    View(labs)
+    rownames(cluster.id.table) = fips.codes
+    return(cluster.id.table)
+  }
+}
+
+MakeClusterColumn = function(cluster.list, labs, county.pop){
+  Cluster.id = rep(0, length(labs))
+  for(i in 1:length(cluster.list)){
+    curr.clust = unlist(cluster.list[[i]])
+    for(j in 1:length(curr.clust)){
+      Cluster.id[curr.clust[j]] = i
+    }
+  }
 }
 
 ListLinks = function(cluster.list, labs, adj.mat, dist.threshold, county.pop){
@@ -150,6 +198,8 @@ ListLinks = function(cluster.list, labs, adj.mat, dist.threshold, county.pop){
   links$"srcpop" = s.population
   links$"tarpop" = t.population
   
+  links = as.data.frame(links)
+  links = links[order(links$pct, decreasing=TRUE),]
   return(links)
 }
 
